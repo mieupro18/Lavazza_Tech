233c54f5607dd08c0d82ae04d39e0edbc0e6c494
Subash-G <>
2020-07-22 13:54:13 +0530

    indentation

--- App.js
+++ App.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 
 
 import StackNavigator from './Navigation/stackNavigation';
-import { View, Text } from 'native-base';
 
-
-import {SafeAreaView} from 'react-native'
 
 class App extends Component {
   constructor(props) {
@@ -13,18 +10,11 @@ class App extends Component {
     this.state = {  
       
     }
-
-  
-
-    
   }
   
   render() { 
     return (
       <StackNavigator/>
-
-      
-      
       );
   }
 }
