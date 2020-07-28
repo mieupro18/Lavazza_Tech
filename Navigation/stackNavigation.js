bbb737d754cc050caec78c7b0e179b5eaad12d2a
Subash-G <>
2020-07-28 19:06:05 +0530

    login page at initial

--- Navigation/stackNavigation.js
+++ Navigation/stackNavigation.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import AsyncStorage from '@react-native-community/async-storage';
 import Login from '../Login/login';
 
 
 import BottomNavigation from './bottomNavigation';
 
 
-
-const token = AsyncStorage.getItem('lavazzaLoginToken').then(value => {console.log(value); return value;} );
-
-const RouteName = (token === null || token === undefined)?('Login'):('Lavazza');
-
 const AppNavigator = createStackNavigator(
   {
     Login: Login,
-    Lavazza: BottomNavigation,
+    LavAzza: BottomNavigation,
   },
   {
-    initialRouteName: RouteName,
+    initialRouteName: 'Login',
   },
 );
 
