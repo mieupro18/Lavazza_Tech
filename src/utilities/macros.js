f10e331fee67710bd6f80f9a92d6bc76ca637750
Jaskar <jaskarj@mieupro.com>
2020-09-12 00:07:15 +0530

    update

--- src/utilities/macros.js
+++ src/utilities/macros.js
@@ -1,5 +1,5 @@
-//const SERVER_URL = 'http://34.123.104.148:9876';
-const SERVER_URL = 'http://192.168.5.1:9876';
+const SERVER_URL = 'http://34.123.104.148:9876';
+//const SERVER_URL = 'http://192.168.5.1:9876';
 const TOKEN = 'l@vA@zzacfd$';
 const SUCCESS = 'Success';
 const FAILURE = 'Failure';
